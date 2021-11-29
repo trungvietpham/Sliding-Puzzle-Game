ffe2c4c8e138e9b7a3c94124d8e3936528f07d28
VIET.PT190074@sis.hust.edu.vn <VIET.PT190074@sis.hust.edu.vn>
2021-11-29 14:23:20 +0700

    Update GUI

--- Interface/giaodien.py
+++ Interface/giaodien.py
@@ -6,240 +6,198 @@ import tkinter.filedialog
 import os
 
 
+
 class Tiles():
-    def __init__(self,  grid_row, grid_col):
-        self.tiles = []
-        self.grid_col = grid_col
-        self.grid_row = grid_row
-        self.gap = None 
-        self.moves = 0
+  def __init__(self, grid):
+    self.tiles = []
+    self.grid = grid
+    self.gap = None  #define in func setGap below
+    self.moves=0
 
-    def add(self, tile):
-        self.tiles.append(tile)
+  def add(self, tile):
+    self.tiles.append(tile)
 
-    def getTile(self, *pos):
-        for tile in self.tiles:
-            if tile.pos == pos:
-                return tile
+  def getTile(self, *pos):
+    for tile in self.tiles:
+      if tile.pos == pos:
+          return tile
 
-    def getTileAroundGap(self):
-        gRow, gCol = self.gap.pos
-        return self.getTile(gRow, gCol - 1), self.getTile(gRow, gCol + 1), self.getTile(gRow - 1, gCol), self.getTile(
-            gRow + 1, gCol)
+  def getTileAroundGap(self):
+    gRow, gCol = self.gap.pos
+    return self.getTile(gRow, gCol-1),self.getTile(gRow, gCol+1),self.getTile(gRow-1, gCol),self.getTile(gRow+1, gCol) 
 
-    def changeGap(self, tile):
-        try:
-            gPos = self.gap.pos
-            self.gap.pos = tile.pos
-            tile.pos = gPos
-            self.moves += 1
-        except:
-            pass
+  def changeGap(self, tile):
+    try:  
+      gPos = self.gap.pos
+      self.gap.pos = tile.pos
+      tile.pos = gPos
+      self.moves+=1
+    except:
+      pass
 
- 
-    def slide(self, key):
-        left, right, top, down = self.getTileAroundGap()
-        if key == 'Up': self.changeGap(down)
-        if key == 'Down': self.changeGap(top)
-        if key == 'Right': self.changeGap(left)
-        if key == 'Left': self.changeGap(right)
-        if key == 's': self.shuffle()
-        self.show()
+  # slide gap follow keyboard, and compare
+  def slide(self, key):
+    left, right, top, down = self.getTileAroundGap()
+    if key=='Up': self.changeGap(down)
+    if key=='Down': self.changeGap(top)
+    if key=='Right': self.changeGap(left)
+    if key=='Left': self.changeGap(right)
+    self.show()
 
 
-    def shuffle(self):
-        random.shuffle(self.tiles)
-        i = 0  
-        for row in range(self.grid_row):
-            for col in range(int(self.grid_col)):
-                self.tiles[i].pos = (row, col)
-                i += 1
-    def shup(self):
-        i = 0 
-        for row in range(self.grid_row):
-            for col in range(int(self.grid_col)):
-                self.tiles[i].pos = (row, col)
-                i += 1
-    def show(self):
-        for tile in self.tiles:
-            if self.gap != tile:
-                tile.show()
 
 
-    def setGap(self, index):
-        self.gap = self.tiles[index]
+  # shuffle function need to be update, check validate shuffle
+  def shuffle(self):
+      random.shuffle(self.tiles)
+      i=0 #iter
+      for row in range(self.grid):
+        for col in range(self.grid):
+            self.tiles[i].pos=(row,col)
+            i+=1
 
-    def isCorrect(self):
+  def show(self):
+    for tile in self.tiles:
+      if self.gap!=tile:
+        tile.show()
 
-        for tile in self.tiles:
-            if not tile.isCorrectPos(): return False
-        return True
+  # define missing tile? 
+  def setGap(self, index): 
+    self.gap = self.tiles[index]
+
+
+  def isCorrect(self):
+    for tile in self.tiles:
+        if not tile.isCorrectPos(): return False
+    return True
+
+
 
 
 class Tile(Label):
-    def __init__(self, parent, tileImage, pos):
-        Label.__init__(self, parent, image=tileImage)
+  def __init__(self, parent, tileImage, pos):
+    Label.__init__(self, parent, image = tileImage)
 
-        self.image = tileImage
-        self.pos = pos
-        self.curPos = pos
+    self.image = tileImage
+    self.pos = pos
+    self.curPos = pos
 
-    def show(self):
-        self.grid(row=self.pos[0], column=self.pos[1])
+  def show(self):
+    self.grid(row = self.pos[0], column=self.pos[1])
 
-    def isCorrectPos(self):
-        return self.pos == self.curPos
-
+  def isCorrectPos(self):
+    return self.pos==self.curPos
 
 class Board(Frame):
-    MAX_BOARD_SIZE = 500
+  MAX_BOARD_SIZE = 500
+  def __init__(self, parent, image, grid, win, *args, **kwargs):
+    Frame.__init__(self, parent, *args, **kwargs)
 
-    def __init__(self, parent, image, grid_row, grid_col,win,sf, *args, **kwargs):
-        Frame.__init__(self, parent, *args, **kwargs)
-        self.parent = parent
-        self.grid_col = grid_col
-        self.grid_row = grid_row
-        self.win = win
-        self.image = self.openImage(image)
-        self.menu()
-        self.tileSize = self.image.size[0] / self.grid_row
-        self.tiles = self.createTiles()
+    self.parent = parent
+    self.grid = grid  
+    self.win = win
+    self.image = self.openImage(image)
+    self.tileSize = self.image.size[0]/self.grid
+    self.tiles = self.createTiles()
+    self.tiles.shuffle()  #shuffle tiles
+    self.tiles.show()
+    self.bindKeys()
 
-        self.tiles.show()
-        self.bindKeys()
-        self.shuffle(sf)
-    def getTiles(self):
-        return self.tiles
-    def shuffle(self,sf):
-        if sf ==True:
-            self.tiles.shuffle()
-            self.tiles.show()
-    def openImage(self, image):
-        image = Image.open(image)  
-        imageSize = min(image.size)  
-        if imageSize > self.MAX_BOARD_SIZE:  
-            image = image.resize((self.MAX_BOARD_SIZE, self.MAX_BOARD_SIZE), Image.ANTIALIAS) 
-        if image.size[0] > image.size[1]:
-            image = image.crop((0, 0, image.size[1], image.size[1]))
-        elif image.size[0] < image.size[1]:
-            image = image.crop((0, 0, image.size[0], image.size[0]))
-        return image
+  def openImage(self, image):
+    image = Image.open(image) #Image here is import at line 3
+    imageSize = min(image.size)  #get size of image
+    if imageSize>self.MAX_BOARD_SIZE:  #if size too big
+        image = image.resize((self.MAX_BOARD_SIZE, self.MAX_BOARD_SIZE), Image.ANTIALIAS)  #then resize it 
+    if image.size[0] > image.size[1]:
+        image = image.crop((0,0, image.size[1], image.size[1]))
+    elif image.size[0]< image.size[1]: 
+        image = image.crop((0,0, image.size[0], image.size[0]))
+    return image
 
+  # function get keyboard event
+  def bindKeys(self):
+    self.bind_all('<Key-Up>', self.slide)
+    self.bind_all('<Key-Down>', self.slide)
+    self.bind_all('<Key-Left>', self.slide)
+    self.bind_all('<Key-Right>', self.slide)
 
-    def bindKeys(self):
-        self.bind_all('<Key-Up>', self.slide)
-        self.bind_all('<Key-Down>', self.slide)
-        self.bind_all('<Key-Left>', self.slide)
-        self.bind_all('<Key-Right>', self.slide)
-        self.bind_all('<s>', self.slide)
+  #function handle event?   
+  def slide(self, event):
+    self.tiles.slide(event.keysym)
 
-    def slide(self, event):
-        self.tiles.slide(event.keysym)
-        
-        if self.tiles.isCorrect():
-            self.win(self.tiles.moves)
-    def donothing(self):
-        a=100
-    def menu(self):
-        a =100
-         
-    def createTiles(self):
+    #check if correct or not
+    if self.tiles.isCorrect():
+        self.win(self.tiles.moves)
 
-        tiles = Tiles(self.grid_row,self.grid_col/2)  
-    
-        for row in range(self.grid_row):
-            for col in range(int(self.grid_col/2)):
-
-                x0 = col * self.tileSize
-                y0 = row * self.tileSize
-                x1 = x0 + self.tileSize
-                y1 = y0 + self.tileSize
-                tileImage = ImageTk.PhotoImage(self.image.crop((x0, y0, x1, y1)))
-                tile = Tile(self, tileImage, (row, col))  
-                tiles.add(tile)
-        tiles.setGap(-1)
-  
-        for row in range(self.grid_row):
-            i = 0
-            for col in range(int(self.grid_col/2),self.grid_col):
-
-                x0 = i * self.tileSize
-                i+=1
-                y0 = row * self.tileSize
-                x1 = x0 + self.tileSize
-                y1 = y0 + self.tileSize
-                img = self.image.crop((x0, y0, x1, y1))
-                tileImage = ImageTk.PhotoImage(img)
-                tile = Tile(self, tileImage, (row, col)) 
-                tile.show()
-
-        return tiles
+  # function split default image into n^2 pieces
+  def createTiles(self):
+    tiles = Tiles(self.grid) #Tiles is a class define above
+    for row in range(self.grid):
+        for col in range(self.grid):
+            x0 = col*self.tileSize
+            y0 = row*self.tileSize
+            x1 = x0+self.tileSize
+            y1 = y0+self.tileSize
+            tileImage = ImageTk.PhotoImage(self.image.crop((x0, y0, x1, y1)))
+            tile = Tile(self, tileImage, (row, col)) #Tile is a class define above
+            tiles.add(tile)
+    tiles.setGap(-1)  #can we pass variable into here? 
+    return tiles
 
 
 class Main():
-    def __init__(self, parent):
-        self.parent = parent
-        self.image = StringVar()
-        self.winText = StringVar()
-        self.grid = IntVar()
-        self.createWidgets()
-        self.sf = False
-    def createWidgets(self):
+  def __init__(self, parent):
+    self.parent = parent
 
-        self.mainFrame = Frame(self.parent)
-        Label(self.mainFrame, text='Sliding Puzzle', font=('ink Free', 65)).pack(padx=10, pady=10)  
-        frame = Frame(self.mainFrame)
-        Label(frame, text='Images',font=('ink Free',15)).grid(sticky=W)
-        Entry(frame, textvariable=self.image, width=50).grid(row=0, column=1, padx=10, pady=10)
-        Button(frame, text='Browse', command=self.browse).grid(row=0, column=2, pady=10, padx=10)
+    self.image = StringVar()
+    self.winText = StringVar()
+    self.grid = IntVar()
 
-        Label(frame, text='Grid',font=('ink Free',15)).grid(sticky=W)
-        OptionMenu(frame, self.grid, *[2, 3, 4, 5, 6, 7, 8, 9, 10]).grid(row=1, column=1, padx=10, pady=10, sticky=W)
-        frame.pack(padx=10, pady=10)
+    self.createWidgets()
 
-        Button(self.mainFrame, text='Start',command=self.start).pack(padx=10, pady=10)
-        Button(self.mainFrame, text='Exit', command=self.exit).pack(padx=10, pady=10)
-        Button(self.mainFrame, text='Shuffle', command=self.shuffle).pack(padx=20, pady=10)
-        self.mainFrame.pack()
-        self.board = Frame(self.parent)
-        self.winFrame = Frame(self.parent)
-        Label(self.winFrame, textvariable=self.winText, font=('', 50), padx=10, pady=10)
-        Label(self.winFrame, text='you win',font=('ink Free',50),fg='red').pack(padx=10, pady=10)
-        Button(self.winFrame, text='Play Again', command=self.playAgain).pack(padx=10, pady=10)
-    def start(self):
-        self.root = Toplevel()
-        image = self.image.get()
-        self.mainFrame1 = Frame(self.root)
-        grid_col, grid_row = self.grid.get(), self.grid.get()
-        if os.path.exists(image):
-            self.board = Board(self.root, image, grid_row, grid_col*2, self.win,self.sf)
-            self.mainFrame1.pack_forget()
-            self.board.pack()
+  def createWidgets(self):
+    self.mainFrame = Frame(self.parent)
+    Label(self.mainFrame, text = 'Sliding Puzzle', font=('', 50)).pack(padx = 10, pady=10)  #pad: offset from center?
+    frame = Frame(self.mainFrame)
+    Label(frame, text = 'Images').grid(sticky=W)
+    Entry(frame, textvariable = self.image, width = 50).grid(row=0, column=1, padx=10, pady=10)
+    Button(frame, text='Browse', command=self.browse).grid(row=0, column=2, pady=10, padx= 10)  
 
-        self.root.mainloop()
-    def exit(self):
-      root.destroy()
-    def shuffle(self):
-        self.sf = True
-        self.board.shuffle(self.sf)
-        self.sf = False
+    Label(frame, text = 'Grid').grid(sticky=W)
+    OptionMenu(frame, self.grid ,*[2,3,4,5,6,7,8,9,10]).grid(row=1, column=1, padx=10, pady=10, sticky=W)
+    frame.pack(padx=10,pady=10)
+
+    Button(self.mainFrame, text='Start',command=self.start).pack(padx=10, pady=10)
+    self.mainFrame.pack()
+    self.board = Frame(self.parent)
+    self.winFrame = Frame(self.parent)
+    Label(self.winFrame, textvariable=self.winText, font=('', 50), padx=10, pady=10)
+    Button(self.winFrame, text='Play Again', command=self.playAgain).pack(padx=10, pady=10)
+
+  # define function run algo?   
+  def start(self):
+    image = self.image.get()
+    grid = self.grid.get()
+    if os.path.exists(image):
+        self.board = Board(self.parent, image, grid, self.win)
+        self.mainFrame.pack_forget()
+        self.board.pack()
 
 
-    def browse(self):
-        self.image.set(tkinter.filedialog.askopenfilename(title="Select Image",initialdir="../", filetypes=(("png file","*.png" ), ("jpg File", "*.jpg"))))
+  # define browse image function:
+  def browse(self):
+      self.image.set(tkinter.filedialog.askopenfilename(title="Select Image", filetypes=(("png file","*.png" ), ("jpg File", "*.jpg"))))
 
-    def win(self, moves):
-        self.board.pack_forget()
-        self.winFrame.pack()
+  def win(self, moves):
+    self.board.pack_forget()
+    self.winText.set('You are win after {0} moves'.format(moves))
+    self.winFrame.pack()
 
-    def playAgain(self):
-        self.board.pack_forget()
-        self.winFrame.pack_forget()       
-        self.mainFrame.pack()
-
+  def playAgain(self):
+    self.winFrame.pack_forget()
+    self.mainFrame.pack()
 
 if __name__ == "__main__":
-    root = Tk()
-    root.title('GR6')
-    Main(root)
-    root.mainloop()
+  root=Tk()
+  Main(root)
+  root.mainloop()
---
+++ Interface/main_menu.py
@@ -0,0 +1,147 @@
+import pygame, sys
+from pygame.constants import K_ESCAPE, KEYDOWN, MOUSEBUTTONDOWN
+
+import play_game
+
+width = 800
+height = 600
+
+
+clock = pygame.time.Clock()
+pygame.init()
+pygame.display.set_caption("Sliding Puzzle")
+screen = pygame.display.set_mode((width, height), 0)
+background_color = (253, 253, 101)
+BLACK = (0, 0, 0)
+YELLOW = (255, 247, 0)
+
+
+def draw_text(text, font, color, surface, x_coor, y_coor):
+    textObject = font.render(text, True, color)
+    textRect = textObject.get_rect()
+    textRect.center = (x_coor, y_coor)
+    surface.blit(textObject, textRect)
+
+
+def button(
+    text_content,
+    font,
+    surface,
+    dx,
+    dy,
+    width=200,
+    height=50,
+    text_color=(255, 255, 255),
+    button_color=(255, 0, 0),
+):
+    btn = pygame.Rect(dx, dy, width, height)
+    pygame.draw.rect(surface, button_color, btn)
+    draw_text(
+        text_content, font, text_color, surface, dx + width // 2, dy + height // 2
+    )
+    return btn
+
+
+def main_menu():
+
+    click = False  # check if click on button or not
+
+    while True:
+
+        mx, my = pygame.mouse.get_pos()
+
+        screen.fill(background_color)
+        x_main_menu, y_main_menu = 300, 100
+        draw_text("MAIN MENU",pygame.font.SysFont("san", 40),(0, 0, 0),screen,x_main_menu,y_main_menu,)
+
+        btn1 = button("New game",pygame.font.SysFont("san", 30),screen,200,y_main_menu + 50,)
+        btn2 = button("About",pygame.font.SysFont("san", 30),screen,200,y_main_menu + 150,)
+
+        # if(btn1.collidepoint((mx, my))):
+        #     if click:
+        #         play_game()
+        #         click = False
+
+        # if btn2.collidepoint((mx,my)):
+        #     if click:
+        #         pass
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+            if event.type == MOUSEBUTTONDOWN:
+                if btn1.collidepoint((mx, my)):
+                    play()
+
+                if btn2.collidepoint((mx, my)):
+                    about()
+
+            if event.type == pygame.KEYUP:
+                if event.key == K_ESCAPE: 
+                    pygame.quit()
+                    sys.exit()
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+def play():
+
+    running = True
+
+    while running:
+        screen.fill(background_color)
+        offset = 50
+        btn = []
+
+        font = pygame.font.SysFont("san", 20)
+        draw_text("Choose size of board:",pygame.font.SysFont("san", 40),BLACK,screen,width//2,100)
+        for i in range(2, 6): 
+            text = str(i)+"x"+str(i)
+            btn.append(button(text, font, screen, (2*i-3)*width//8- offset, 200, 100, 40))
+        
+        for i in range (6, 10):
+            text = str(i)+"x"+str(i)
+            btn.append(button(text, font, screen, (i*2-11)*width//8- offset, 250, 100, 40))
+
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYUP:
+                if event.key == K_ESCAPE:
+                    running = False
+            if event.type == MOUSEBUTTONDOWN:
+                for i in range(0,8): 
+                    if btn[i].collidepoint(pygame.mouse.get_pos()):
+                        play_game.main(i+2)
+
+        pygame.display.update()
+        clock.tick(60)
+
+
+def about():
+    # thông tin thành viên nhóm
+    running = True
+
+    while running:
+        screen.fill(background_color)
+        draw_text("About: ",pygame.font.SysFont("times", 30),(0, 0, 0),screen,width//2,100)
+        draw_text("Các thành viên trong nhóm bao gồm: ",pygame.font.SysFont("times", 20),(0, 0, 0),screen,width//2,150)
+        draw_text("1. Phạm Trung Việt ",pygame.font.SysFont("times", 20),(0, 0, 0),screen,width//2,200)
+        draw_text("2. Nguyễn Thị Thùy Trang ",pygame.font.SysFont("times", 20),(0, 0, 0),screen,width//2,250)
+        draw_text("3. Quách Thế Trường ",pygame.font.SysFont("times", 20),(0, 0, 0),screen,width//2,300)
+        draw_text("4. Trần Văn Nghĩa ",pygame.font.SysFont("times", 20),(0, 0, 0),screen,width//2,350)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYUP:
+                if event.key == K_ESCAPE:
+                    running = False
+
+        pygame.display.update()
+        clock.tick(60)
+
+if __name__ == "__main__":
+    main_menu()
